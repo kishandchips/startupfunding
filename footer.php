4b89e56a0257f606c6736052f94754df6bff9bc0
Spyros Lefkaditis <slefkaditis@gmail.com>
2015-03-10 10:10:40 +0000

    Request Changes
    - Changed homepage CTA size
    - Added footer disclaimer
    
    Signed-off-by: Spyros Lefkaditis <slefkaditis@gmail.com>

--- footer.php
+++ footer.php
@@ -29,6 +29,12 @@
 					<?php endwhile;endif; ?>
 				</div>
 			</div>
+			<div class="disclaimer column col-full">
+				<p>Startup Funding Club Limited ("SFC") is not regulated by the Financial Conduct Authority and is not a registered broker
+				or any other regulated entity. SFC does not sell or offer to sell any securities. Neither SFC nor its personnel are providing
+				financial advice in relation to any investments. We therefore recommend that companies seek financial advice from an independent
+				financial advisor authorised pursuant to the Financial Services and Markets Act 2000.</p>
+			</div>
 		</div>
 	</div>
 </footer>
